9b926f89de1ad8b5be1d37088903a158c5e9d821
Agustina <agustinagauna@gmail.com>
2020-04-13 21:24:30 -0300

    .

---
+++ src/main/java/ar/com/ada/sb/security/jwt/model/mapper/circular/dependency/UserCycleMapper.java
@@ -0,0 +1,11 @@
+package ar.com.ada.sb.security.jwt.model.mapper.circular.dependency;
+
+import ar.com.ada.sb.security.jwt.model.dto.UserDto;
+import ar.com.ada.sb.security.jwt.model.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring", uses = {})
+public interface UserCycleMapper extends DataCycleMapper<UserDto, User> {
+    UserCycleMapper MAPPER = Mappers.getMapper(UserCycleMapper.class);
+}
---
+++ src/main/java/ar/com/ada/sb/security/jwt/model/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package ar.com.ada.sb.security.jwt.model.repository;
+
+import ar.com.ada.sb.security.jwt.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+}
